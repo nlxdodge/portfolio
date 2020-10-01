54febf4fe4abb122d39a68390419e6ebe6ea1698
Gerben Veenhof <nlxdodge@gmail.com>
2020-10-01 13:08:47 +0200

    Fixes for aria-label on wrong spot

--- source/_layouts/work.blade.php
+++ source/_layouts/work.blade.php
@@ -5,9 +5,9 @@
   <p class="italic">Made by: {{ $page->author }} - last update: {{ date('j-F-Y', $page->date) }}</p>
   @yield('content')
   <div class="center-text margin-top-50">
-    <a class="button" href="/posts"><i aria-hidden="true" aria-label="All works" class="fas fa-chevron-circle-left"></i> All works</a>
+    <a class="button" href="/posts" aria-label="All works"><i aria-hidden="true" class="fas fa-chevron-circle-left"></i> All works</a>
     @if ($page->link)
-      <a class="button" href="{{ $page->link }}"><i aria-hidden="true" aria-label="Link" class="fas fa-link"></i> Link</a>
+      <a class="button" href="{{ $page->link }}" aria-label="Link"><i aria-hidden="true" class="fas fa-link"></i> Link</a>
     @endif
   </div>
 </article>
