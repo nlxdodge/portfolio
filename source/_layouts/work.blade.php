20d0b5959e0b977cce34530e28499f176f1a4ba7
Gerben Veenhof <nlxdodge@gmail.com>
2020-10-28 09:36:34 +0100

    Bugfix changed url

--- source/_layouts/work.blade.php
+++ source/_layouts/work.blade.php
@@ -5,7 +5,7 @@
   <p class="italic">Made by: {{ $page->author }} - last update: {{ date('j-F-Y', $page->date) }}</p>
   @yield('content')
   <div class="center-text margin-top-50">
-    <a class="button" href="/posts" aria-label="All works"><i aria-hidden="true" class="fas fa-chevron-circle-left"></i> All works</a>
+    <a class="button" href="/works" aria-label="All works"><i aria-hidden="true" class="fas fa-chevron-circle-left"></i> All works</a>
     @if ($page->link)
       <a class="button" href="{{ $page->link }}" aria-label="Link"><i aria-hidden="true" class="fas fa-link"></i> Link</a>
     @endif
