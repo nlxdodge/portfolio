a355b42556efb12f5e0f9a3bdcf7bdf9a07d8d95
NLxDoDge <nlxdodge@gmail.com>
2021-02-16 14:41:35 +0100

    remove google tag manager

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -25,20 +25,6 @@
     <!-- scripts and styles -->
     <style>{{ inline(mix('css/main.css', 'assets/build')) }}</style>
     <script defer type="text/javascript" src="{{ mix('js/main.js', 'assets/build') }}"></script>
-
-    @if ($page->production)
-    <!-- Google Analytics (Yeah I know not really for your privacy) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-83869367-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'UA-83869367-1');
-    </script>
-    @endif
 </head>
 
 <body>
