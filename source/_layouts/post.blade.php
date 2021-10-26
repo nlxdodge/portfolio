b703416b75cb535f630e0e923561ccf7805ed69e
Gerben Veenhof <gerben.veenhof@sogeti.com>
2021-10-26 10:36:23 +0200

    Update dates

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -2,7 +2,7 @@
 @section('body')
 <article class="post">
   <h1>{{ $page->title }}</h1>
-  <p class="italic">Writen by: {{ $page->author }} - Last update: {{ date('j-F-Y', $page->date) }}</p>
+  <p class="italic">Writen by: {{ $page->author }} - Last update: {{ date('j F Y', $page->date) }}</p>
   @yield('content')
   <div class="center-text margin-top-50">
     <a class="button" href="/posts" aria-label="All posts"><i aria-hidden="true" class="fas fa-chevron-circle-left"></i> All posts</a>
--- source/_layouts/work.blade.php
+++ source/_layouts/work.blade.php
@@ -2,7 +2,7 @@
 @section('body')
 <article class="work">
   <h1>{{ $page->title }}</h1>
-  <p class="italic">Made by: {{ $page->author }} - last update: {{ date('j-F-Y', $page->date) }}</p>
+  <p class="italic">Made by: {{ $page->author }} - last update: {{ date('j F Y', $page->date) }}</p>
   @yield('content')
   <div class="center-text margin-top-50">
     <a class="button" href="/works" aria-label="All works"><i aria-hidden="true" class="fas fa-chevron-circle-left"></i> All works</a>
