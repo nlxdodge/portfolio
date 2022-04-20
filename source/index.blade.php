34352909d0594744f978378565ed6ee901b5f3ac
NLxDoDge <nlxdodge@gmail.com>
2022-04-20 23:15:21 +0200

    Change more text

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,6 +4,16 @@
 <div>
   <h1>Backend redefined</h1>
   <p>Welcome to my humble site, you can find all sorts of informational posts and my proud works. While also giving a sneak peek for you to try it out yourselves.</p>
+  <h2>How it al started</h2>
+  <p>I Started of my career with interests in computer science at a very young age. And took that to school where I choose to be an aspiring game developer. But soon
+    found out that making games was almost the same as writing code for websites or other applications. Hence, I started to learn a new and higher degree for application
+    development. At which I am running my last year of right now.</p>
+  <p>With a short list of some of my experiences:</p>
+  <ul>
+    <li>Backend | Java, Ruby, Pyton, PHP, C# (Unity), C# .NET</li>
+    <li>Frontend | Vue & Angular with NPM & Yarn</li>
+    <li>Cloud | AWS (Cloudformation, cloudfront, lambda's, gateway integration etc.)</li>
+  </ul>
 </div>
 
 <div class="glide">
@@ -44,16 +54,6 @@
 </div>
 
 <div>
-  <h2>How it al started</h2>
-  <p>I Started of my career with interests in computer science at a very young age. And took that to school where I choose to be an aspiring game developer. But soon
-    found out that making games was almost the same as writing code for websites or other applications. Hence, I started to learn a new and higher degree for application
-    development. At which I am running my last year of right now.</p>
-  <p>With a short list of some of my experiences:</p>
-  <ul>
-    <li>Backend | Java, Ruby, Pyton, PHP, C# (Unity), C# .NET</li>
-    <li>Frontend | Vue & Angular with NPM & Yarn</li>
-    <li>Cloud | AWS (Cloudformation, cloudfront, lambda's, gateway integration etc.)</li>
-  </ul>
   <h3>Want more info about me?</h3>
   <p>Sent me a message though one of the following social media outlets to get my full <span class="italic">Curriculum Vitae.</span></p>
 </div>
