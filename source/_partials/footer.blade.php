310f0f4d11357d0c4c9af6857f3cadbbd1db595b
NLxDoDge <nlxdodge@gmail.com>
2022-07-29 17:57:16 +0200

    Remove reddit as it is a bit to casual mabye

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -5,7 +5,6 @@
             <a href="https://github.com/nlxdodge" target="_blank" aria-label="Github link"><i aria-hidden="true" class="fab fa-github-square"></i></i></a>
             <a href="https://www.linkedin.com/in/gerbenveenhof/" target="_blank" aria-label="Linkedin link"><i aria-hidden="true" class="fab fa-linkedin"></i></a>
             <a href="https://twitter.com/gerbenveenhof" target="_blank" aria-label="Twitter link"><i aria-hidden="true" class="fab fa-twitter-square"></i></a>
-            <a href="https://www.reddit.com/user/nlxdodge" target="_blank" aria-label="Reddit link"><i aria-hidden="true" class="fab fa-reddit-square"></i></a>
         </div>
         <p>
             <i aria-hidden="true" aria-label="Copyright" class="fas fa-copyright"></i> Gerben Veenhof | {{ date("Y") }} | Made with <a class="jigsaw-link" href="https://jigsaw.tighten.co/" target="_blank" aria-label="Heart">Jigsaw <i aria-hidden="true" class="fas fa-heart"></i></a>
